050d09bfff05f0b7fffccd40e933b5e9aeb08522
PradeepMugithe <pmugithe@iu.edu>
2024-03-05 16:13:49 -0500

    Push

---
+++ 03-colors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "Color coding is important to find out what's the message means"
+
+#A script can use escape sequences to produce colored text on the terminal.
+
+#Colors for text are represented by color codes, 
+#including, 
+#reset = 0, black = 30, red = 31, green = 32, yellow = 33, blue = 34, magenta = 35, cyan = 36, and white = 37.
+
+#For Example: echo -e "\e[31m This is red text \e[0m"
+
+echo -e "\e[31m This is red text \e[0m"
+
+
