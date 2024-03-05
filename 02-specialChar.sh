6fd0abde32d27cbeff9b967a6350650f7c594b95
PradeepMugithe <pmugithe@iu.edu>
2024-03-05 16:08:02 -0500

    Push

--- 02-specialChar.sh
+++ 02-specialChar.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo This file is about special characters particularly escape 
+echo This file is about special characters particularly escape sequences 
 
 #\n new line
-# use -e to enable the escape sequence char features in quotes with out this, you dont see expected output
+# use -e to enable the escape sequence char features in double quotes with out this, you dont see expected output
+#\t tab space
 
 echo -e "Line1\nLine2" 
+
+echo -e "Cloud\tDevops"
