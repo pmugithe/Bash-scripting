ac7e80b0fe22bbe9d21d27a563f6ab6f01d3ade5
PradeepMugithe <pmugithe@iu.edu>
2024-03-05 15:30:10 -0500

    Updated sample file

---
+++ 01-sample.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Shebang Notation: This tells the scrip to which interpretor it should use.
+#There can be only one interpretor per script
+
+echo Welcome to Bash Scripting
+
+#This is how we execute the bash file
+#$ bash scriptName.sh or sh schriptName.sh or  ./scriptName.sh
+
