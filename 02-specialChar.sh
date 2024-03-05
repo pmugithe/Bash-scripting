eb9a17c0ef3f94771a2c050ccd4e211b81aee96f
PradeepMugithe <pmugithe@iu.edu>
2024-03-05 16:03:14 -0500

    Push

---
+++ 02-specialChar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo This file is about special characters particularly escape 
+
+#\n new line
+# use -e to enable the escape sequence char features in quotes with out this, you dont see expected output
+
+echo -e "Line1\nLine2" 
