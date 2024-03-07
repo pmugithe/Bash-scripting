e691062f9708fde59e40a77339bfa052043ba03f
PradeepMugithe <pmugithe@iu.edu>
2024-03-07 11:53:56 -0500

    Push

---
+++ 06-specialVaraibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Technical aspects of Special variables asked in interviews
+
+# to find out the name of scripts executing
+# Like $0 to $9, $?, $#, $*, $@
+
+# $0 prints name of the scripts executing
+
+echo "Scripts exeucting now is: " $0
