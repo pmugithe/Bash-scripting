8067fc5a667494bd86aacbe18e4a4801794e2bfb
PradeepMugithe <pmugithe@iu.edu>
2024-03-07 12:35:31 -0500

    Push

---
+++ 09-commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# There are four types of commands available
+
+# Binary /bin - Regular commands, /sbin - secure bin only for root users
+# Aliases Alias or shortcuts 
+# Shell builtin Commands
+# Functions 
+
+# # How to declare a function?
+
+# # f(){
+#         echo Hello
+# #   }
+
+# # to call a function we use f
+
+sampleFunction(){
+    echo This is a sample function
+    echo assume we did run some commands
+    echo now exit 
+}
+
+echo calling a function here 
+sampleFunction
+echo called and exited function in above step
+
