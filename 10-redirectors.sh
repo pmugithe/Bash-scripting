b2fe88617b1dea88070945feec3f5796c7d305f4
PradeepMugithe <pmugithe@iu.edu>
2024-03-07 15:24:27 -0500

    Push

---
+++ 10-redirectors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Redirectors
+
+# Redirectors:
+# 	< Input Redirection
+# 	> Output redirection
+# 	2> for error
+# 	&> both std and error 
+# 	Std output: > or >> or 1
+# 	Std error: 2> or 2>>
+# 	Std output and std error: &> or &>>
+
+ls -ltr
+
