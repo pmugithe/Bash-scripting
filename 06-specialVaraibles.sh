8ba3a3b2050beb3947140703850915dcbc27e9b9
PradeepMugithe <pmugithe@iu.edu>
2024-03-07 12:20:45 -0500

    Push

--- 06-specialVaraibles.sh
+++ 06-specialVaraibles.sh
@@ -12,3 +12,12 @@ echo "Scripts exeucting now is: " $0
 # $1- $9 are the arguments for a bash script example: bash file.sh arg1, arg2, ... arg9
 
 echo "Today is " $1
+
+# $0 specifies the name of the script to be invoked.
+# $1-$9 stores the names of the first 9 arguments or can be used as the arguments' positions.
+# $# specifies the total number (count) of arguments passed to the script.
+# $* stores all the command line arguments by joining them together.
+# $@ stores the list of arguments as an array.
+# $$ specifies the process ID of the current script.
+# $? specifies the exit status of the last command or the most recent execution process.
+# $! shows ID of the last background job.
---
+++ 07-exitCodes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Exit codes in linux
+
+# Each and every action in Linux will have a exit code
+
+# 0-255 is the range 
+# 0- Action Completed
+# 1 t0 255 -  partial failure, partial success, or complete failure
