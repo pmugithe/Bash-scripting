16f7674ad0e40a99d890f8de85b5a1f87bdccd06
PradeepMugithe <pmugithe@iu.edu>
2024-03-07 11:58:17 -0500

    Push

--- 06-specialVaraibles.sh
+++ 06-specialVaraibles.sh
@@ -8,3 +8,7 @@
 # $0 prints name of the scripts executing
 
 echo "Scripts exeucting now is: " $0
+
+# $1- $9 are the arguments for a bash script example: bash file.sh arg1, arg2, ... arg9
+
+echo "Today is " $1
