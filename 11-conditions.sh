cfed2b3bccdb6807a9ca466dac142d224a80e075
PradeepMugithe <pmugithe@iu.edu>
2024-03-08 14:00:13 -0500

    Push

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -19,5 +19,9 @@ case $ACTION in
         echo "In the stop condition" ;;
     restart)
         echo "Restart condition" ;;
+    *) #if start, stop or rstart were not give, print not a valid, chose start, stop or restart
+        echo "Invalid Options"
+        echo "Choose start, stop or restart" 
+        ;;
 esac
 
