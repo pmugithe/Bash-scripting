2e1e2e03dfe722f9a8ab0d96d6d6edc8a6768b22
PradeepMugithe <pmugithe@iu.edu>
2024-03-08 16:18:14 -0500

    Push

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -9,27 +9,27 @@
 #     opt 2) some commands;;
 # esac
 
-ACTION=$1
+# ACTION=$1
 
-case $ACTION in
-    start)
-        echo "In the starting condition"
-        exit 0
-        ;;
-    stop) 
-        echo "In the stop condition"
-        exit 1
-        ;;
-    restart)
-        echo "Restart condition" 
-        exit 2
-        ;;
-    *) #if start, stop or rstart were not give, print not a valid, chose start, stop or restart
-        echo "Invalid Options"
-        echo "Choose start, stop or restart" 
-        exit 3
-        ;;
-esac
+# case $ACTION in
+#     start)
+#         echo "In the starting condition"
+#         exit 0
+#         ;;
+#     stop) 
+#         echo "In the stop condition"
+#         exit 1
+#         ;;
+#     restart)
+#         echo "Restart condition" 
+#         exit 2
+#         ;;
+#     *) #if start, stop or rstart were not give, print not a valid, chose start, stop or restart
+#         echo "Invalid Options"
+#         echo "Choose start, stop or restart" 
+#         exit 3
+#         ;;
+# esac
 
 
 echo "Do you know Java Programming?"  
