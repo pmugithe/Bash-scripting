75109af713fba5f5ad0d34705a7286d2644fd39b
PradeepMugithe <pmugithe@iu.edu>
2024-03-10 15:12:28 -0400

    Files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "This is a Frontend script file"
+
+yum install nginx -y
+echo "Front End"
+
+# yum install nginx -y
+# systemctl enable nginx
+# systemctl start nginx
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
