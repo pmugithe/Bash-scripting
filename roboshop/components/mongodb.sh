2f19421926bb642bcb0b8d01d0b654da71e780a2
PradeepMugithe <pmugithe@iu.edu>
2024-03-12 15:38:54 -0400

    Files

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -35,7 +35,7 @@ yum install -y mongodb-org &>> ${LOGFILE}
 stat $?
 
 echo "Enabling the ${COMPONENT} visibility..."
-sed -ie 's/127.0.0.1/0.0.0.0/g' /ect/mongod.conf
+sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 stat $?
 
 echo -n "Starting the ${COMPONENT} component..."
