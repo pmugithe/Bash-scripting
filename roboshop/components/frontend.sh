455629ba47cb46ca9df8d8cf20e81a5f06e5d4b1
PradeepMugithe <pmugithe@iu.edu>
2024-03-12 10:57:03 -0400

    Files

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,34 +9,32 @@ if [ $USER_ID -ne 0 ]; then
     echo -e "\e[31m Please login with valid user or sudo user\e[0m \n \t Example: sudo bash wrapper.sh frontendh.sh"
     exit 1
 fi
+
+# Function to check the status
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[32m Failure \e[0m"
+    fi
+
+}
+
 echo -n "Configuring Frontend: "
 yum install nginx -y &>> /tmp/frontend.log
 
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[32m Failure \e[0m"
-fi
+stat $?
 
 echo -n "Starting nginx..."
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx  &>> /tmp/frontend.log
-
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[32m Failure \e[0m"
-fi
-
+stat $?
 echo -n "Downloading the frontend component..."
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -e "\e[32m Failure \e[0m"
-fi
+stat $?
 
 # cd /usr/share/nginx/html
 
