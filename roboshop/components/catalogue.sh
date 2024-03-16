921d2483bd096472efa4a0971fe933fd04abb89c
PradeepMugithe <pmugithe@iu.edu>
2024-03-16 11:21:00 -0400

    Files

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,7 +3,7 @@
 COMPONENT = catalogue
 
 # This is how we import the functions that are declared in a different file using source 
-source /common.sh
+source ./common.sh
 NODEJS           # calling nodejs function.
 
 echo "Installation is copmpleted..."
