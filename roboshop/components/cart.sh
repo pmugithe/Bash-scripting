6055a4519c943662f261c02f60103897d4c3b183
PradeepMugithe <pmugithe@iu.edu>
2024-04-08 00:20:49 -0400

    files

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "This is a Cart script file"
+
+
+COMPONENT=cart
+
+# This is how we import the functions that are declared in a different file using source 
+source ./common.sh
+NODEJS                       # calling nodejs function.
+
+echo -e "\n \e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
