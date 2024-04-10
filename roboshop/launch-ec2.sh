712c98cf297bc1ab1dcc553f98271c239d7cd5d2
PradeepMugithe <pmugithe@iu.edu>
2024-04-10 15:57:35 -0400

    files

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -19,7 +19,10 @@ fi
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
 
 INSTANCE_TYPE="t2.micro"
-SG_ID=sg-09d812ab7c313a2ed
+# SG_ID=sg-09d812ab7c313a2ed
+
+SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=cloud-devops-sgs | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
+
 
 
 # aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type ${INSTANCE_TYPE}\
