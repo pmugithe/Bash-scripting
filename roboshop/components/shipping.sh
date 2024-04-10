94b099ebe31d0bef8236d7bc55c0b6a774be1beb
PradeepMugithe <pmugithe@iu.edu>
2024-04-10 10:16:38 -0400

    files

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "This is a Shipping script file"
+
+COMPONENT=shipping
+
+# This is how we import the functions that are declared in a different file using source 
+source ./common.sh
+JAVA                       # calling nodejs function.
+
+echo -e "\n \e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
