ebcce4d8888e6be77e259902f3039742a793845a
PradeepMugithe <pmugithe@iu.edu>
2024-04-10 10:15:14 -0400

    files

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "This is a Payment script file"
+
+
+COMPONENT=payment
+
+# This is how we import the functions that are declared in a different file using source 
+source ./common.sh
+PYTHON                       # calling nodejs function.
+
+echo -e "\n \e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
