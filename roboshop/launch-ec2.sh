0ade2e130560e9b99dee32756a666200437b37df
PradeepMugithe <pmugithe@iu.edu>
2024-04-10 20:08:59 -0400

    files

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -32,4 +32,11 @@ SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=clou
 echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
 PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE} --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g') 
 
- echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
+echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
+
+# echo -e "Creating DNS Record of ${COMPONENT}: "
+
+# sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
+
+# aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
+# echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
